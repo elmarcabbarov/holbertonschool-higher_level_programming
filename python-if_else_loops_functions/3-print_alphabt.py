4ea3f983b2e7e8a5121a5591c002feb06a93fb08
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-13 14:58:01 +0400

    This is a commit

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-for i in range(97, 123):  # ASCII values for 'a' to 'z'
-    if chr(i) != 'e' and chr(i) != 'q':
-        print("{}".format(chr(i)), end="")
-                    
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")                  
