546d3ff6c73a73cb45e7f0086ab7579186e5597b
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-14 14:44:49 +0400

    This is a commit

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-for i in range(1,100):
-    if i < 10:
-        print("0"+str(i), end =",")
+for i in range(0,100):
+    if i < 99:
+        if i < 10:
+            i="0"+str(i)
+        else:
+            i=str(i)
+        print(i, end =", ")
     elif i == 99:
         print(str(i))
-    else:
-        print(str(i), end=",")
