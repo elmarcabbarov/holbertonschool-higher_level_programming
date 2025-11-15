6348e88f862e9a7556832f1dc1dddaae9dcdedf1
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-15 11:53:32 +0000

    This is a commit

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -2,10 +2,10 @@
 def islower(c):
     """
     Checks if a character is lowercase.
-    
+
     Args:
         c: A character to check
-        
+
     Returns:
         True if c is lowercase, False otherwise
     """
