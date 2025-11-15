4568d3ed3bb718f7d2421c97e09182c0291f31cd
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-15 11:50:57 +0000

    This is a commit

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+def islower(c):
+    """
+    Checks if a character is lowercase.
+    
+    Args:
+        c: A character to check
+        
+    Returns:
+        True if c is lowercase, False otherwise
+    """
+    return ord('a') <= ord(c) <= ord('z')
