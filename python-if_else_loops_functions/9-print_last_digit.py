8bcfc87df7966495432498a5d067d44a761c4895
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-15 12:01:22 +0000

    This is a commit

---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    """
+    Prints the last digit of a number.
+
+    Args:
+        number: An integer
+
+    Returns:
+        The value of the last digit
+    """
+    last_digit = abs(number) % 10
+    print("{:d}".format(last_digit), end="")
+    return last_digit
