7d831198fc3091d669b503865303cdcd74ef0e41
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-18 09:42:16 +0000

    This is a commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,3 +1,7 @@
-#!/usr/bin/python3 
-def add(a, b):
-    return a + b
+#!/usr/bin/python3
+from add_0 import add
+a = 1
+b = 2
+
+if __name__ == "__main__":
+    print("{} + {} = {}".format(a, b, add(a, b)))
