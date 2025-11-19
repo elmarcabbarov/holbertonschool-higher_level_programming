796ef3191928d1283049d9f862cf00b0649dea0d
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-19 12:19:28 +0000

    This is a commit

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     my_list = list(my_string)
+    result = ""
     for i in my_list:
-        if i !="c" or i !="C":
-           result += i
+        if i !="c" and i != "C":
+           result = result + i
     return result
