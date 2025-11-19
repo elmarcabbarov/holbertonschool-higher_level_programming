cec4818f88ec2608c17e58503e21352077847024
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-19 11:55:41 +0000

    This is a commit

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element)
+    new_list = my_list
     if idx > 0 or idx <= len(my_list):
-     new_in_list = my_list[idx] = element
-     return new_in_list
+        new_list[idx] = element
+        return new_list
+    else:
+        return my_list
