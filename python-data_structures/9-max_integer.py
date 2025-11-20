e17728480c15cda78fb1e2c3138145a6060d612b
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-20 17:25:16 +0400

    This is a commit

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -3,8 +3,8 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
+        a = my_list[0]
         for i in my_list :
-            a = my_list[0]
             if i > a:
                 a = i
             else:
