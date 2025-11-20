b24f647ca944953a1b2eaa38b1abc63e02fe1e2b
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-20 17:52:02 +0400

    This is a commit

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    new_my_list = []
     if idx < 0 or idx >= len(my_list):
         return my_list
-    else:
-        for i in range(len(my_list)):
-            if i == idx :
-                continue
-            else:
-                new_my_list.append(my_list[i])
-    
-    my_list = new_my_list
+    my_list[:] = my_list[:idx] + my_list[idx + 1:]
     return my_list
