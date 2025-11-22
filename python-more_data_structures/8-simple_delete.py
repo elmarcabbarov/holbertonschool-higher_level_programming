8ffffecd191852a8271b5434bf645b8997969000
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-22 10:30:09 +0000

    This is a commit

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -4,4 +4,4 @@ def multiply_by_2(a_dictionary):
     for key, value in a_dictionary.items():
         v = value * 2
         b_dictionary[key] = v
-    return b_dictionary, a_dictionary
+    return b_dictionary
