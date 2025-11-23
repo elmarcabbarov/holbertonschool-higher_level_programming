6034286cb99f6cb6b7e3761bc8ff110dc859bfc3
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-23 12:53:42 +0000

    This is a commit

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    """Divides two integers and prints the result."""
+    result = None
+    try:
+        result = float(a) / float(b)
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
