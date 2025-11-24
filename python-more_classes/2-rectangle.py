22f98a30cfbf868bde3a9a1e9bdd17de43476d8e
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-24 19:58:30 +0000

    This is a commit

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -41,11 +41,11 @@ class Rectangle:
         self.__height = value
     
     def area(self):
-        "'"" Calculate the area """
+        "'""Calculate the area"""
         return self.__width * self.__height
     
     def perimeter(self):
-        """ Calculate the perimetr """
+        """Calculate the perimetr"""
         if self.__height == 0 or self.__width == 0:
             return 0
         return (self.__width +self.__height) * 2
