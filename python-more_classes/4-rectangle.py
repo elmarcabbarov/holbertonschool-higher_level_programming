ad1b92fc4055b7f9bf2390a1d5fafbf13f3279ba
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-25 12:02:03 +0400

    This is a commit

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -61,7 +61,7 @@ class Rectangle:
 
     def __repr__(self):
         """return a string representation of the rectangle to be able to recreate a new instance"""
-    return "Rectangle({}, {})".format(self.__width, self.__height)
+        return "Rectangle({}, {})".format(self.__width, self.__height)
     
     def __eval__(self):
         """Create the rectangle"""
