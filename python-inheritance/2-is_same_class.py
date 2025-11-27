832f6f7dc075b0ae0a3ea7803e0d478f1a9375e9
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-27 08:45:02 +0000

    This is a commit

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Bu taskda verilenin tipini yoxlayan funksiya yazilib
+Function that checks if an object is exactly an instance of a given class.
 """
 
 
 def is_same_class(obj, a_class):
     """
-    tipi yoxlayir True or Flase qyatarir
+    Return True if obj is exactly an instance of a_class, otherwise False.
     """
     return type(obj) == a_class
