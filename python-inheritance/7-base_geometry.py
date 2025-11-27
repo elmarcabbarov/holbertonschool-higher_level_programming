fd1f93e403e6b2dd3bef22e927e5799881ae6718
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-27 10:09:58 +0000

    This is a commit

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Bu taskda bos bir class yaradilir
+"""
+
+
+class BaseGeometry():
+    """
+    Bos bir kals yaratdim
+    """
+
+    def area(self):
+        """
+        Exception qaldiririq nesede cixis verir
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        TyepError ValueError fln qaldiri
+        """
+        if type(value) is not int:
+            raise TypeError("f{name}must be an integer")
+        if value <= 0:
+            raise ValueError("f{name}must be greater than 0")
