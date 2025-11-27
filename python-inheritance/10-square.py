3fbc7c6269d951b90a0b581e313a4c3fea4436e9
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-27 10:52:40 +0000

    This is a commit

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """square to inherit from rectangle"""
 Rectangle = __import__('9-rectangle').Rectangle
-        
+
 
 class Square(Rectangle):
     """
-    Square 
+    Square
     """
+
     def __init__(self, size):
         """
         ilkin melumat cart curt
         """
         self.integer_validator("size", size)
-        self.__size =size
+        self.__size = size
         super().__init__(size, size)
