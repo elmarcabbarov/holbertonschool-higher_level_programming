cc0b78d9b828b0824e9356e39009ac9cb8538385
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-27 13:37:02 +0000

    This is a commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+    Fayli oxuayn kod
+"""
+
+
 def read_file(filename=""):
+    """
+    Bu funkisya fayli oxuyur
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
