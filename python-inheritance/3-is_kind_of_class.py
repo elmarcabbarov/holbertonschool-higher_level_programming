c289178df18e0df4ee7d4166e603635ad5a4e308
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-27 09:27:23 +0000

    This is a commit

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Bu taskda verilenin classi yoxlayan funksiya yazilib
+"""
+
+def is_kind_of_class(obj, a_class):
+    """
+    Nese qaytarir
+    """
+    return isinstance(obj,a_class)
