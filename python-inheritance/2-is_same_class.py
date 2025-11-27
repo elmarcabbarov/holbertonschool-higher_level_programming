a29288f0597f39dad7d068cea42977f87780220a
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-27 08:40:58 +0000

    This is a commit

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Bu taskda verilenin tipini yoxlayan funksiya yazilib
+"""
+
+def is_same_class(obj, a_class):
+    """
+    tipi yoxlayir True or Flase qyatarir
+    """
+    return type(obj) == a_class
