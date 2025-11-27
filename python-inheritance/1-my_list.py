cbd4d8e1abcf2753f7c69e3a2833411e2efc1bca
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-27 08:17:10 +0000

    This is a commit

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Bu taskda listden miras alib kod yaziriq
+"""
+
+
+class MyList(list):
+    """
+    List miras alir sonra funksiya yaradir
+    """
+    def print_sorted(self):
+        """ 
+        sortlanmis lsit print edir
+        """
+        print(sorted(self))
