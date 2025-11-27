c8b954343a341b0678835473ba5aa79db335a22b
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-27 09:46:52 +0000

    This is a commit

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Bu taskda bos bir class yaradilir
+"""
+
+
+class BaseGeometry():
+    """
+    Bos bir kals yaratdim
+    """
+
+    def area(self):
+        """
+        Exception qaldiririq nesede cixis verir
+        """
+        raise Exception("")
