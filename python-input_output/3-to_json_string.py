f82b44dfea5b8bbfd681f395691a450b6a2034af
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-27 20:46:00 +0000

    This is a commit

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -12,4 +12,4 @@ def to_json_string(my_obj):
     Jsona çeviren funksiya
     """
     json_string = json.dumps(my_obj, ensure_ascii=False, indent=2)
-    xreturn json_string
+    return json_string
