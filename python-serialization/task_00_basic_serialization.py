c7e336c340b9708693250b04606b03045bb08263
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-29 17:31:33 +0000

    This is a commit

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Serilasiya ve Deserilasiya eden kod
+"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Python dict-i JSON faylına yazır.
+    Mövcud fayl varsa, əvəz olunur.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(data, f)
+
+
+def load_and_deserialize(filename):
+    """JSON faylından dict oxuyur."""
+    with open(filename, "r", encoding="utf-8") as f:
+        json.load(f)
