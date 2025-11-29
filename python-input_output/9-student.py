184ba178e4e6a275fc5d2d1b5578dd1899966484
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-29 11:14:21 +0000

    This is a commit

---
+++ python-input_output/9-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""student classi yarat sonra icindekileri json ele deyesen"""
+
+
+class Student():
+    """Public deyerler yarat"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """8 dekinen eynidi"""
+        return self.__dict__
