596c8a595b4cb0db39ed034ad8d3cd76b0377bde
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-29 19:27:31 +0000

    This is a commit

---
+++ python-serialization/task_02_csv.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+CSV den JSONA
+"""
+
+import csv
+import json
+
+
+def convert_csv_to_json(csv_filename):
+    """ CSVnin oxunmasi"""
+    try:
+        with open(csv_filename, "r", encoding="utf-8") as f:
+            csv_reader = csv.DictReader(f)
+            data = []
+            for row in csv_reader:
+                data.append(row)
+
+        """CSVnin jsona elave edilmesi"""
+        with open("data.json", "wb", encoding="utf-8") as j:
+            json.dump(data, j)
+        return True
+
+        """Eks halda False qyatarir"""
+    except BaseException:
+        return False
