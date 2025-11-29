1090442b49dd0b704d42e2276077d41032363b23
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-29 09:05:19 +0000

    This is a commit

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,6 +11,5 @@ def save_to_json_file(my_obj, filename):
     """
     Writes an object to a file in JSON format
     """
-    data = json.dumps(my_obj)
     with open(filename, "w", encoding="utf-8") as f:
-        f.write(data)
+        f.write(json.dumps(my_obj)
