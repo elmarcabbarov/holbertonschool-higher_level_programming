504958e12de7f0292fe335c15aabb5535ba1f355
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-29 18:18:52 +0000

    This is a commit

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -17,16 +17,16 @@ class CustomObject():
 
     """Melumatlarin cap edilmesi"""
 
-    def display(self, name, age, is_student):
-        print(f"Name: {name}")
-        print(f"Age: {age}")
-        print(f"Is Student: {is_student}")
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
     """Serilasiya"""
 
     def serialize(self, filename):
         try:
-            with open(filename, "r", encoding="utf-8") as f:
+            with open(filename, "wb") as f:
                 return pickle.dump(self, f)
         except Exception:
             return None
@@ -35,7 +35,7 @@ class CustomObject():
     @classmethod
     def deserialize(cls, filename):
         try:
-            with open(filename, "rb", encoding="utf-8") as f:
+            with open(filename, "rb") as f:
                 return pickle.load(f)
         except BaseException:
             return None
