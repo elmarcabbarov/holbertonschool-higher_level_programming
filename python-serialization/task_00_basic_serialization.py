6407d9d7c38be041d863b2e5b81d533399a56a6b
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-29 17:40:40 +0000

    This is a commit

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -12,10 +12,10 @@ def serialize_and_save_to_file(data, filename):
     Mövcud fayl varsa, əvəz olunur.
     """
     with open(filename, "w", encoding="utf-8") as f:
-        json.dump(data, f)
+        return json.dump(data, f)
 
 
 def load_and_deserialize(filename):
     """JSON faylından dict oxuyur."""
     with open(filename, "r", encoding="utf-8") as f:
-        json.load(f)
+        return json.load(f)
