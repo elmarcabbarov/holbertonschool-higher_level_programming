9429ea4e7fb33fc9914ad4f4671c94c38cf0acc7
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-30 14:01:06 +0000

    This is a commit

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,3 +1,2 @@
 -- Yeni sutun elave eden kod
-ALTER TABLE first_table ADD id INT(89);
-ALTER TABLE first_table ADD name VARCHAR("BEST SCHOOL");
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
