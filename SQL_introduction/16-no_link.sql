50c75209e437f0f0ef013f18d7ca7e3a90618dc4
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-30 15:19:42 +0000

    This is a commit

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- second_tableda score ve namei gosteren kod
-SELECT score, name FROM second_table WHERE name IF NOT NULL AND name !='' ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name !='' ORDER BY score DESC;
