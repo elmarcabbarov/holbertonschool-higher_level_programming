26fd2de0b87e217256ad0740e821d702ecc5ca46
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-11-30 16:07:33 +0000

    This is a commit

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,5 @@
 -- databas ve user yaradan kod
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED "user_0d_2_pwd";
+GRANT SELECT ON hbtn_0d_2.* TO "user_0d_2"@"localhost";
+FLUSH PRIVILEGES;
