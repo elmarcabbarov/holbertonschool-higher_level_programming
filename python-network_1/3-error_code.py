da8ab1eb93236c7c9d5497fb3ef01f36eed9f8ba
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-12-03 12:38:16 +0400

    This is a commit

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Import etdik"""
+import urllib.request, sys
+
+"""Validation"""
+
+if len(sys.argv) < 2:
+    print()
+else:
+    url = sys.argv[1]
+"""Try except"""
+    try:
+        with urllib.request.urlopen(url) as r:
+           print(r.read().decode("utf-8"))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
