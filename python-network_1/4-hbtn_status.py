d182a5efbfd6475f81919d2cf4322bc5054ddc86
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-12-03 15:03:45 +0400

    This is a commit

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -3,5 +3,6 @@
 import requests
 """Melumati elde et ve cap et"""
 r = requests.get("https://intranet.hbtn.io/status")
+r = r.text
 print("\t- type: {}".format(type(r)))
 print("\t- type: {}".format(r))
