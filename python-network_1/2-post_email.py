489772b3c553d0fe017b9b932a148c3889ea33f5
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-12-03 12:19:57 +0400

    This is a commit

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""import et"""
+import urllib.request
+import urllib.parse
+import sys
+
+"""Yoxlanish"""
+if len(sys.argv) < 2:
+    print()
+
+else:
+    """Ne bilim nese"""
+    url = sys.argv[1]
+    mail = sys.argv[2]
+    mail = {"email": mail}
+    byte = urllib.parse.urlencode(mail).encode("utf-8")
+    with urllib.request.urlopen(url, data=byte) as r:
+        body = r.read()
+    print(body)
