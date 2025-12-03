202ff4bc8e36bb96ed6d50fac74dd4bb9b4dd4d8
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-12-03 15:05:10 +0400

    This is a commit

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -5,4 +5,4 @@ import requests
 r = requests.get("https://intranet.hbtn.io/status")
 r = r.text
 print("\t- type: {}".format(type(r)))
-print("\t- type: {}".format(r))
+print("\t- content: {}".format(r))
