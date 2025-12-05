e50995793cafc102f72e9703e37310287fb730cc
elmarcabbarov <elmarcabbarov2005@gmail.com>
2025-12-05 13:14:54 +0400

    This is a commit

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
+"""Send POST request to search_user with letter in q variable"""
 
-"""Post gondermek requests kitabxanasi ile"""
-
-
-import requests as r
-
+import requests
 import sys
 
+# 1. URL
+url = "http://0.0.0.0:5000/search_user"
 
+# 2. q dəyəri
 if len(sys.argv) > 1:
     q = sys.argv[1]
 else:
     q = ""
 
-url = "http://0.0.0.0:5000/search_user"
+# 3. POST sorğusu
+response = requests.post(url, data={'q': q})
 
-nermin = {"q": q}
-
+# 4. JSON emalı
 try:
-    req = r.post(url, data=nermin, headers={'cfclearance': 'true'})
-    data = req.json()
+    data = response.json()
     if not data:
         print("No result")
     else:
